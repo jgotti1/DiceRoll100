fdf075060387d888451e9e62c2868573f488779e
jgotti1 <jgotti1@optonline.net>
2022-04-27 10:56:24 -0400

    v3 JS clean up

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ bntHoldEl.addEventListener('click', function () {
   scores[activePlayer] += currentScore;
   document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
-  if (scores[activePlayer] >= 10) {
+  if (scores[activePlayer] >= 100) {
     document
       .querySelector(`.player--${activePlayer}`)
       .classList.add('player--winner');
